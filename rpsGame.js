1db0d5322719fd98a3f2264c1c7e01a8f8b057b3
sethdame <seth.e.dame@gmail.com>
2015-11-04 07:10:48 -0700

    closer

--- rpsGame.js
+++ rpsGame.js
@@ -5,81 +5,81 @@ $(document).ready(function() {
     })
 })
 
-var wins = 0;
-var losses = 0;
-var ties = 0;
+// var wins = 0;
+// var losses = 0;
+// var ties = 0;
 
-var again = function() {
-    var playAgain = window.confirm("Do you want to play again?");
-    if (playAgain == true) {
-        rpsGame();
-    } else {
-        alert("Thanks for playing!");
-    }
-}
-var tie = function() {
-    ties += 1;
-    document.getElementById("tie").innerHTML = ties;
-    confirm("Tie! Play again!");
-    rpsGame(); 
-}
-var wrongChoice = function() {
-    confirm("Choose again!");
-    rpsGame();
-}
-var win = function() {
-    wins += 1;
-    document.getElementById("win").innerHTML = wins;
-    confirm("You Win!");
-    again();
-}
-var lose = function() {
-    losses += 1;
-    document.getElementById("loss").innerHTML = losses;
-    confirm("I beat you!");
-    again();
-}
+// var again = function() {
+//     var playAgain = window.confirm("Do you want to play again?");
+//     if (playAgain == true) {
+//         rpsGame();
+//     } else {
+//         alert("Thanks for playing!");
+//     }
+// }
+// var tie = function() {
+//     ties += 1;
+//     document.getElementById("tie").innerHTML = ties;
+//     confirm("Tie! Play again!");
+//     rpsGame(); 
+// }
+// var wrongChoice = function() {
+//     confirm("Choose again!");
+//     rpsGame();
+// }
+// var win = function() {
+//     wins += 1;
+//     document.getElementById("win").innerHTML = wins;
+//     confirm("You Win!");
+//     again();
+// }
+// var lose = function() {
+//     losses += 1;
+//     document.getElementById("loss").innerHTML = losses;
+//     confirm("I beat you!");
+//     again();
+// }
 
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return tie();
-    } else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
-            return win();
-        } else {
-            return lose();
-        }
-    } else if (choice1 === "paper") {
-        if (choice2 === "rock") {
-            return win();
-        } else {
-            return lose();
-        }
-    } else if (choice1 === "scissors") {
-        if (choice2 === "rock") {
-            return lose();
-        } else {
-            return win();
-        }
-    } else if (choice1 != "scissors", "rock", "paper") {
-        return wrongChoice();
-    }
-}
+// var compare = function(choice1, choice2) {
+//     if (choice1 === choice2) {
+//         return tie();
+//     } else if (choice1 === "rock") {
+//         if (choice2 === "scissors") {
+//             return win();
+//         } else {
+//             return lose();
+//         }
+//     } else if (choice1 === "paper") {
+//         if (choice2 === "rock") {
+//             return win();
+//         } else {
+//             return lose();
+//         }
+//     } else if (choice1 === "scissors") {
+//         if (choice2 === "rock") {
+//             return lose();
+//         } else {
+//             return win();
+//         }
+//     } else if (choice1 != "scissors", "rock", "paper") {
+//         return wrongChoice();
+//     }
+// }
 
-var rpsGame = function() {
-    var userChoice = prompt("Do you choose rock, paper or scissors?");
-    var computerChoice = Math.random();
-    if (computerChoice < 0.34) {
-        computerChoice = "rock";
-    } else if(computerChoice <= 0.67) {
-        computerChoice = "paper";
-    } else {
-        computerChoice = "scissors";
-    } console.log("Computer: " + computerChoice);
+// var rpsGame = function() {
+//     var userChoice = prompt("Do you choose rock, paper or scissors?");
+//     var computerChoice = Math.random();
+//     if (computerChoice < 0.34) {
+//         computerChoice = "rock";
+//     } else if(computerChoice <= 0.67) {
+//         computerChoice = "paper";
+//     } else {
+//         computerChoice = "scissors";
+//     } console.log("Computer: " + computerChoice);
 
-    compare(userChoice, computerChoice);
-}
-rpsGame();
+//     compare(userChoice, computerChoice);
+// }
+// rpsGame();
 
 ////////PLAY AGAIN FROM BUTTON/////////////
 
@@ -119,14 +119,14 @@ var playAgain = function() {
     "Do you choose rock, paper or scissors?";
 }
 var compareAgain = function(choice1, choice2) {
-    var rock = "rock";
-    var paper = "paper";
-    var scissors = "scissors";
+    // var rock = "rock";
+    // var paper = "paper";
+    // var scissors = "scissors";
 
-    var choice2 = Math.random();
-    if (choice2 < 0.34) {
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
         choice2 = "rock";
-    } else if(choice2 <= 0.67) {
+    } else if(computerChoice <= 0.67) {
         choice2 = "paper";
     } else {
         choice2 = "scissors";
